6f67ac6e70d8296da01432df6ca9894dd95ff8c8
Osama <osama.abdo4040@gmail.com>
2023-10-07 14:39:59 +0300

    initial project. added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/home/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName : (context) => HomeScreen(),
+      },
+    );
+  }
+}
